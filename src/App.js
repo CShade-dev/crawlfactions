a493381791576a442f52f1738c9378ce7bd9be2e
Caleb Shade <pokesm4shvids@gmail.com>
2020-04-10 21:52:20 -0400

    Finished the desktop version of the site.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import './App.css';
-import 'typeface-roboto';
 import logo from '../src/assets/Crawl.png';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
-
+import discord from '../src/assets/discord.png';
+import shop from '../src/assets/shop.png';
+import key from '../src/assets/key.png';
 
 function App() {
   function copyFunc() {
     let tooltip = document.getElementById("tooltip")
     tooltip.innerHTML = "Copied!"
-    setTimeout(() => {tooltip.innerHTML = "Click to copy"}, 6000)
+    setTimeout(() => {tooltip.innerHTML = "Click to copy"}, 2000)
   }
+  function navFunc () {
+    const el = document.querySelector("nav");
+    el.classList.toggle("change");
+  };
   return (
     <>
     <div className="logodiv">
-    <img src={logo} className="logo" />
+    <img src={logo} className="logo" alt="CrawlFactions Logo"/>
     <CopyToClipboard text="play.crawlfactions.com">
     <p className="serverip" id="serverip" onClick={copyFunc}>play.CrawlFactions.com
     <span className="tooltip" id="tooltip">Click to copy</span>
@@ -23,6 +28,47 @@ function App() {
     </div>
     <div className="App">
     </div>
+    <nav>
+    <div className="hamburger" onClick={navFunc}>
+      <div className="bar1"></div>
+      <div className="bar2"></div>
+      <div className="bar3"></div>
+    </div>
+    <div className="navigation">
+      <div className="links">
+        <a href="http://crawlfactions.tebex.io/" className="shoplink">
+          <div className="shop">
+            <div className="shopicon">
+              <img className="shopimg" src={shop} alt="Click to visit our shop." />
+            </div>
+            <div className="shoptext">
+              <p className="shopp">Click here to go to our shop page!</p>
+            </div>
+          </div>
+        </a>
+        <a href="https://linkslist.app/IVSE3CH" className="votelink">
+          <div className="vote">
+            <div className="voteicon">
+              <img src={key} className="keylogo" alt="Click here to view our voting sites." />
+            </div>
+            <div className="votetext">
+              <p className="votep">Click here to see a list of our voting links!</p>
+            </div>
+          </div>
+        </a>
+        <a className="discordlink" href="https://discord.gg/q2by2Tj">
+          <div className="discord">
+            <div className="discordicon">
+              <img src={discord} className="discordlogo" alt="Click here to join our discord." />
+            </div>
+            <div className="discordtext">
+              <p className="discordp">Click here to go to our discord!</p>
+            </div>
+          </div>
+        </a>
+        </div>
+    </div>
+    </nav>
     </>
   );
 }
