31464c58d2dbe448b5a48475a0d3f89233da6e87
Caleb Shade <pokesm4shvids@gmail.com>
2020-05-06 01:39:40 -0400

    Updates

---
+++ precache-manifest.5e4053a76e1fb1584425072354679478.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3f35c1e61f41b7b1dd6201eac01d11",
+    "url": "/crawlfactions/index.html"
+  },
+  {
+    "revision": "537bce83b7bb1ebb5367",
+    "url": "/crawlfactions/static/css/main.27812bdb.chunk.css"
+  },
+  {
+    "revision": "6bf291cc09d30b038d90",
+    "url": "/crawlfactions/static/js/2.12a106aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crawlfactions/static/js/2.12a106aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "537bce83b7bb1ebb5367",
+    "url": "/crawlfactions/static/js/main.db26945d.chunk.js"
+  },
+  {
+    "revision": "5a6c66a22cba79786de4",
+    "url": "/crawlfactions/static/js/runtime-main.eac3fc4a.js"
+  },
+  {
+    "revision": "06fb146fe97ccb2710d1bc9c4dfede6f",
+    "url": "/crawlfactions/static/media/Crawl.06fb146f.png"
+  },
+  {
+    "revision": "6e02ec1a88191ec82b79988bbe105871",
+    "url": "/crawlfactions/static/media/background.6e02ec1a.jpg"
+  },
+  {
+    "revision": "b884e30a55ac81d4b712ec7e037a293f",
+    "url": "/crawlfactions/static/media/shop.b884e30a.png"
+  }
+]);
